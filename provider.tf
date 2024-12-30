3ec93ecd84c3793b0c6454cf9a4220944f20eef7
randiveshreyas3053 <shreyas.s.randive2002@gmail.com>
2024-12-30 13:13:55 +0000

    FCcccc

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  
+  access_key = "AKIATXZ5EBI6PDESZPW6"
+  secret_key = "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"
+  
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "key-name" {
+
+}
+
+variable "network-security-group-name" {
+
+}
+
+variable "debian-ami" {
+
+}
+
+variable "debian-instance-type" {
+
+}
