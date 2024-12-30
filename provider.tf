d137bc9746c261c0d319d33dfb4f5de2f967a3f7
randiveshreyas3053 <shreyas.s.randive2002@gmail.com>
2024-12-30 13:26:30 +0000

    asda

--- provider.tf
+++ provider.tf
@@ -2,7 +2,10 @@
 provider "aws" {
   region = "ap-south-1"
   
-  access_key = "AKIATXZ5EBI6PDESZPW6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   
 }
+
+#AKIATXZ5EBI6PDESZPW6
+#Ij8sNagIhp5bRXziCdp08SwPSSuHLRbjK1q1Zidm
