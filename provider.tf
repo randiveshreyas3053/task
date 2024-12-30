dc2cf35ac01ad0ec3a2fccba631caa4b2f502470
randiveshreyas3053 <shreyas.s.randive2002@gmail.com>
2024-12-30 13:52:48 +0000

    mamammmm

--- provider.tf
+++ provider.tf
@@ -1,9 +1,11 @@
+
+
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
   
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIATXZ5EBI6PDESZPW6"
+  secret_key = "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"
   
 }
 
